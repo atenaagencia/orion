408a3558e6a6d4434c6f877846193acdf179b163
herbetjr <herbetjr@gmail.com>
2020-03-17 17:08:19 -0300

    Migrate Associados Criado

---
+++ database/migrations/2020_03_17_194435_create_associados_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssociadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('associados', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->integer('cpf')->default(11);
+            $table->string('rg')->nullable();
+            $table->date('data_nasc');
+            $table->string('nome_mae');
+            $table->string('cep')->nullable();
+            $table->string('rua');
+            $table->string('bairro');
+            $table->string('numero');
+            $table->string('cidade');
+            $table->string('uf');
+            $table->string('email_pessoal');
+            $table->string('email_inst')->nullable();
+            $table->string('escolaridade');
+            $table->string('instituicao');
+            $table->string('turno_aula')->nullable();
+            $table->string('area_formacao')->nullable();
+            $table->string('especializacao')->nullable();
+            $table->longText('talentos')->nullable();
+            $table->string('password');       
+            $table->timestamps();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('associados');
+    }
+}
