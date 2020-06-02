10f81e5818d989e90af6e1f03a17d52c9c017692
philldeveloper <philldeveloper@gmail.com>
2020-06-02 19:16:37 -0300

    0206

--- resources/views/auth/pages/login.blade.php
+++ resources/views/auth/pages/login.blade.php
@@ -13,7 +13,8 @@
 
    <!-- Custom fonts for this template-->
   <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu&family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
   <link href="{{asset('css/sb-admin-2.css')}}" rel="stylesheet">
@@ -22,6 +23,14 @@
 </head>
 
 <style>
+  .roboto-font{
+    font-family: 'Roboto', sans-serif;
+    font-weight: 900;
+  }
+  h1, h2, h3, h4{
+    font-family: 'Ubuntu', 'sans-serif';
+    font-weight: 900;
+  }
   body{
     height: 100vh;
   }
@@ -46,52 +55,30 @@
 
       <div class="col-xl-10 col-lg-12 col-md-9">
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
+        <div class="card o-hidden border-0 shadow-lg my-5 rounded-0">
+          <div class="card-body rounded-0 p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image">
-                <img src="/imagens/orion_logo_branca.png" alt="" class="" style="position:absolute; bottom:0px; width:40%;opacity:40%;">
+              <div class="col-lg-6 d-none bg-light d-lg-block" style="padding-top: 10em;"><!--bg-login-image-->
+                <h1 class="display-4 roboto-font text-purple text-center mx-auto">Orion</h1>
               </div>
               <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    
-                    <h1 class="h4 text-gray-900 mb-4">Bem-vindo de volta cavaleiro!</h1>
-          
-                  </div>
-                <form class="user" method="post" action="/login">
-                    @csrf
-                    <div class="form-group">
-                    <input type="text"  name="cpf" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Informe login" value="{{old('cpf')}}" required>
-                    </div>
-                    <div class="form-group">
-                      <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Informe senha" required>
-                    </div>
-                    {{-- <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                <div class="p-5" id="teste">
+                  <h1 class="d-none d-sm-block d-md-none display-4 roboto-font text-purple text-left">Orion</h1>
+                  <h2 class="text-gray-900">Bem-vindo cavaleiro!</h2><br><br>
+                  <form class="user" method="post" action="/login">
+                      @csrf
+                      <div class="form-group">
+                        <input type="text"  name="cpf" class="form-control form-control-user p-4" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Usuário" value="{{old('cpf')}}" required>
                       </div>
-                    </div> --}}
-                    <button class="btn btn-login-purple btn-user btn-block">
-                      Login
-                    </button>
-                    <hr>
-                    {{-- <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a> --}}
-                  </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="#">Esqueceu a senha?</a>
-                  </div>
-                  {{-- <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
-                  </div> --}}
+                      <div class="form-group">
+                        <input type="password" name="password" class="form-control form-control-user p-4" id="exampleInputPassword" placeholder="Senha" required>
+                      </div>
+                      <button class="btn btn-login-purple btn-user btn-block p-3 mt-5">
+                        <span class="h5 py-auto mx-auto my-auto font-weight-bold">Login</span>
+                      </button>
+                    </form>
+                    
                 </div>
               </div>
             </div>
@@ -101,7 +88,9 @@
       </div>
 
     </div>
-
+    <div class="text-center">
+      <span class="">Um produto de</span> <a class="small font-weight-bold text-light" href="http://www.atenaagencia.com" target="/blank">Atena Agencia</a>.
+    </div>
   </div>
 
   <!-- Bootstrap core JavaScript-->
